e04b8e03f247700cde65524d7e3b4e6ce5ba5d7a
root <you@example.com>
2026-01-18 14:21:36 +0530

    code changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello DevSecOps!");
+  res.send("Hello DevSecOps! Jenkins Pipeline with Docker");
 });
 
 app.listen(3000, () => {
